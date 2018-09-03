edd2d79e02c4a014b7145672fe8fcdacc2fbb27c
shrek301 <tim_lat@mail.ru>
2018-09-03 22:32:52 +0500

    new commit

--- src/main/java/frontend/Frontend.java
+++ src/main/java/frontend/Frontend.java
@@ -8,10 +8,12 @@ import java.io.IOException;
 
 public class Frontend extends HttpServlet {
     private String login = "";
-    public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-public  void  doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        
-}
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
